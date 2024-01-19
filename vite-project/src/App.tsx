5391305d233bfd9195096e6e7db150ef0b07152e
Amador Santiago <amador@Amaadors-MacBook-Air.local>
2024-01-19 17:54:09 -0600

    ACT3 - Amador-S - Showing user data from object

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,33 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Avatar from '@mui/material/Avatar';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const userProfile = { name: 'Amador Santiago', 
+    email: 'amador@example.com', 
+    phone: 81123456789, 
+    address: '21 Main Street, Monterrey Nuevo Leon, CP65343' }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <div className="avatar"><Avatar sx={{ width: 150, height: 150, fontSize: '50px' }}>AS</Avatar></div>
+          </Grid>
+          <Grid item xs={12}>
+           <h1 className="name">{userProfile.name}</h1>
+          </Grid>
+          <Grid item xs={12}>
+            <div className="card-container">
+              <Card sx={{ width: 700}}>
+                <CardContent>
+                  <Typography sx={{ fontSize: 32, }}gutterBottom>
+                    Information
+                  </Typography>
+                  <section className="Information">
+                    <Typography sx={{ fontSize: 24 }}gutterBottom>
+                      Email: {userProfile.email}
+                    </Typography>
+                    <Typography sx={{ fontSize: 24 }}gutterBottom>
+                      Phone: {userProfile.phone}
+                    </Typography>
+                    <Typography sx={{ fontSize: 24 }}gutterBottom>
+                      Addres: {userProfile.address}
+                    </Typography>                                     
+                  </section>                               
+                </CardContent>
+              </Card>
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   )
 }
