c61b923b8add30fb7e61fe9fd080019405d0acdf
Amador Santiago <amador@Amaadors-MacBook-Air.local>
2024-02-04 17:30:08 -0600

    ACT8 - Amador-S - Using the useEffect and useState hook

--- vite-project/src/components/User.tsx
+++ vite-project/src/components/User.tsx
@@ -1,7 +1,21 @@
 import './User.css'
 import UserList from  './UserList'
+import React, { useState, useEffect } from 'react';
+
 
 function User() { 
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    setUsers([
+      { name: 'Amador Santiago', 
+        email: 'amador@example.com', 
+        phone: 81123456789, 
+        address: '21 Main Street, Monterrey Nuevo León, CP65343'
+      }
+    ])
+  }, []); 
 
   const userData = [
     { name: 'Amador Santiago', 
